d04bbbed714898ac7fa513e36c06b47ce402bea4
vleypi <vleypi@gmail.com>
2022-07-29 16:41:36 +0500

    draft changes

--- controllers/draftController.js
+++ controllers/draftController.js
@@ -1,6 +1,6 @@
 const Draft = require('../schema/draft')
 const shortid = require('shortid')
-
+const User = require('../schema/user')
 
 class DraftController {
 
@@ -34,6 +34,25 @@ class DraftController {
         }
     }
 
+    async deleteDraft (req,res){
+        try{
+
+            console.log(1)
+            
+            const draft = await Draft.deleteOne({user: req.user.id, draftid: req.body.data.draftid})
+
+            if(!draft){
+                return res.status(404).json({message: 'Bad request'})
+            }
+            
+            return res.status(200).json({message: ''})
+        }
+        catch(err){
+            console.log(err)
+            return res.status(404).json({message: 'Bad request'})
+        }
+    }
+
     async getDraft (req,res){
         try{
             const draft = await Draft.findOne({user: req.user.id, new: true})
@@ -58,13 +77,34 @@ class DraftController {
 
     async getDraftById (req,res){
         try{
-            const draft = await Draft.findOne({draftid: req.body.draftid,user: req.user.id})
+            const draft = await Draft.findOne({draftid: req.body.draftid,user: req.user.id,})
 
             if(!draft){
                 return res.status(404).json({message: 'Bad request'})
             }
 
             return res.status(200).json({draft})
+        }
+        catch(err){
+            return res.status(404).json({message: 'Bad request'})
+        }
+    }
+
+    async getDraftPageById (req,res){
+        try{
+            const draft = await Draft.findOne({draftid: req.body.draftid,user: req.user.id,new: false})
+
+            if(!draft){
+                return res.status(404).json({message: 'Bad request'})
+            }
+
+            const user = await User.findOne({id: req.user.id},{name: 1,userid: 1})
+
+            if(!user){
+                return res.status(404).json({message: 'Bad request'})
+            }
+
+            return res.status(200).json({draft,profile: user})
         }
         catch(err){
             return res.status(404).json({message: 'Bad request'})
--- routes/draftRouter.js
+++ routes/draftRouter.js
@@ -8,7 +8,9 @@ const auth = require('../mw/auth.mw')
 const auth02 = require('../mw/auth02.mw')
 
 router.post('/saveDraft',auth,draftController.saveDraft)
+router.post('/deleteDraft',auth,draftController.deleteDraft)
 router.get('/getDraft',auth02,draftController.getDraft)
 router.get('/getDraftById',auth02,draftController.getDraftById)
+router.get('/getDraftPageById',auth02,draftController.getDraftPageById)
 
 module.exports = router
